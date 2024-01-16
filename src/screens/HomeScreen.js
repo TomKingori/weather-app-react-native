b12f28ab2de08d13c26433af3cd3e90a400f8a23
TomKingori <tomkingori66@gmail.com>
2024-01-16 15:33:15 +0300

    forecast

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,18 +6,23 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { theme } from "../theme";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
-import { MapPinIcon } from "react-native-heroicons/solid";
+import { CalendarDaysIcon, MapPinIcon } from "react-native-heroicons/solid";
 
 const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
   const [showSearch, toggleSearch] = useState(false);
   const [locations, setLocations] = useState([1, 2, 3]);
+
+  const handleLocation = (loc) => {
+    console.log("location: ", loc);
+  };
 
   return (
     <View className="flex-1 relative">
@@ -55,22 +60,98 @@ export default function HomeScreen() {
           {locations.length > 0 && showSearch ? (
             <View className="absolute w-full top-16 bg-gray-300 rounded-3xl">
               {locations.map((loc, index) => {
-                let showBorder = index+1 != locations.length
-                let borderClass = showBorder? 'border-b-2 border-b-gray-400' : ''
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? "border-b-2 border-b-gray-400"
+                  : "";
                 return (
                   <TouchableOpacity
-                  key={index}
-                  className={"flex-row items-center border-0 p-3 px-4 mb-1 " + borderClass}
+                    onPress={handleLocation(loc)}
+                    key={index}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1 " +
+                      borderClass
+                    }
                   >
-                  <MapPinIcon size="20" color="gray" />
-                    <Text className="text-black text-lg ml-2">Nairobi, Kenya</Text>
+                    <MapPinIcon size="20" color="gray" />
+                    <Text className="text-black text-lg ml-2">
+                      Nairobi, Kenya
+                    </Text>
                   </TouchableOpacity>
                 );
               })}
             </View>
           ) : null}
         </View>
+
+        {/* Forecast */}
+        <View className="mx-4 flex justify-around flex-1 mb-2">
+          {/* Location   */}
+          <Text className="text-white text-center text-2xl font-bold">
+            Nairobi,
+            <Text className="text-lg font-semibold text-gray-300"> Kenya</Text>
+          </Text>
+          {/* Weather image */}
+          <View className="flex-row justify-center">
+            <Image
+              source={require("../../assets/images/partlycloudy.png")}
+              className="h-52 w-52"
+            />
+          </View>
+          {/* Degree Celsius */}
+          <View className="space-y-2">
+            <Text className="text-center font-bold text-white text-6xl ml-5">
+              23&#176;
+            </Text>
+            <Text className="text-center text-white text-xl tracking-widest">
+              Partly cloudy
+            </Text>
+          </View>
+          {/* Other stats */}
+          <View className="flex-row justify-between mx-4">
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/wind.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22km</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/drop.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">23%</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/sun.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
       </SafeAreaView>
+
+      {/* Forecast for next days */}
+      <View className="mb-2 space-y-3">
+        <View className="flex-row items-center mx-5 space-x-2">
+          <CalendarDaysIcon size={22} color={"white"} />
+          <Text className="text-white text-base"> Daily forecast</Text>
+        </View>
+        <ScrollView 
+        horizontal
+        contentContainerStyle={{paddingHorizontal: 15}}
+        showsHorizontalScrollIndicator={false}
+        >
+        <View className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4">
+
+        </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
